d7fe3826e20ab4490725477a3b7429832f71db25
Aurélien GEORGES <aurelien.georges30@gmail.com>
2023-09-28 18:11:35 +0200

    feat: test for netlify

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,7 +4,7 @@ const ImageModal = ({ imageUrl, onClose }) => {
   return (
     <div className="image-modal">
       <div className="modal-content">
-        <img src={imageUrl} alt="Full-screen Image" />
+        <img src={imageUrl} alt="Full-screen size" />
         <button className='btnModal' onClick={onClose}>Close</button>
       </div>
     </div>
