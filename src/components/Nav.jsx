9c4b59ad8cb40d1de299e084a82aeefbb495038e
Aurélien GEORGES <aurelien.georges30@gmail.com>
2023-09-28 09:46:25 +0200

    feat: :sparkles: only the selected list is open for the nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,39 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Nav = ({ onButtonClick }) => {
 
-    function displayOnOff(elm) {
-        if (elm.className === "displayOn") {
-            elm.className = "displayOff";
-        } else {
-            elm.className = "displayOn";
-        }
-    }
+    const [openSection, setOpenSection] = useState(null);
 
-    function displayStructure() {
-        let elm = document.querySelector('#olStructure')
-        displayOnOff(elm)
-    }
-
-    function displayComportement() {
-        const elm = document.querySelector('#olComportement')
-        displayOnOff(elm)
-    }
-
-    function displayMerise() {
-        const elm = document.querySelector('#olMerise')
-        displayOnOff(elm)
+    function displayOnOff(sectionId) {
+        setOpenSection((prevOpenSection) => (prevOpenSection === sectionId ? null : sectionId));
     }
 
     return (
 
         <nav className="nav">
             <div>
-            <a href='#textBeginning'><button onClick={() => onButtonClick('presentation')}>UML & MERISE</button></a>
+                <a href='#textBeginning'><button onClick={() => onButtonClick('presentation')}>UML & MERISE</button></a>
             </div>
             <div>
-                <button onClick={displayStructure}>Diagrammes UML de structure</button>
-                <ol className="navList displayOff" id='olStructure'>
+                <button onClick={() => displayOnOff('olStructure')}>Diagrammes UML de structure</button>
+                <ol className={`navList ${openSection === 'olStructure' ? 'displayOn' : 'displayOff'}`} id='olStructure'>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('class-diagram')}>Diagrammes de classes</button></a></li>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('object-diagram')}>Diagrammes d'Objets</button></a></li>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('componant-diagram')}>Diagrammes de Composants</button></a></li>
@@ -44,8 +27,8 @@ const Nav = ({ onButtonClick }) => {
                 </ol>
             </div>
             <div>
-                <button onClick={displayComportement}>Diagrammes UML de comportement</button>
-                <ol className="navList displayOff" id='olComportement'>
+                <button onClick={() => displayOnOff('olComportement')}>Diagrammes UML de comportement</button>
+                <ol className={`navList ${openSection === 'olComportement' ? 'displayOn' : 'displayOff'}`} id='olComportement'>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('use-case-diagram')}>Diagrammes de Cas d'Utilisation</button></a></li>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('activity-diagram')}>Diagrammes d'Activité</button></a></li>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('transition-state-diagram')}>Diagrammes des Etats-Transitions</button></a></li>
@@ -56,8 +39,8 @@ const Nav = ({ onButtonClick }) => {
                 </ol>
             </div>
             <div>
-                <button onClick={displayMerise}>Diagrammes MERISE</button>
-                <ol className="navList displayOff" id='olMerise'>
+                <button onClick={() => displayOnOff('olMerise')}>Diagrammes MERISE</button>
+                <ol className={`navList ${openSection === 'olMerise' ? 'displayOn' : 'displayOff'}`} id='olMerise'>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('MCD')}>MCD modèle conceptuel des données</button></a></li>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('MPD')}>MPD modèle physique des données</button></a></li>
                     <li><a href='#textBeginning'><button onClick={() => onButtonClick('MLD')}>MLD modèle logique des données</button></a></li>
